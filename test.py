c5c6b04228e556afe899ef228e66649415fdb626
ajravikumar <ajayravikumar007@gmail.com>
2019-12-01 18:32:40 +0000

    V1.0 (Baseline model)

--- test.py
+++ test.py
@@ -5,8 +5,37 @@ import seaborn as sns
 import matplotlib as plt
 import io
 import json
-base_dir='/home/ajay/Documents/fake_news.json';
-df=pd.read_json(base_dir,orient='columns',lines=True);
-# pd.read_json("../input/roam_prescription_based_prediction.jsonl", lines=True, orient='columns')
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+base_dir='/home/ajay/Documents/fake_news_copy.json';
+# READ  DATA
+df=pd.read_json(base_dir);
 print(df.head());
 print('hello');
+
+# SPLIT DATA
+X_all=df.headline;
+Y_all=df.is_sarcastic;
+
+print(X_all.head());
+
+X_train, X_test, Y_train, Y_test=train_test_split(X_all, Y_all,test_size=0.25, random_state=50);
+
+
+# FEATURE EXTRACTION
+vectorizer= TfidfVectorizer(stop_words='english');
+
+tfidf_train= vectorizer.fit_transform(X_train);
+# print(tfidf_train.shape);
+tfidf_test= vectorizer.transform(X_test);
+# print(tfidf_test.shape);
+
+# FITTING AND PREDICTION
+clf=MultinomialNB();
+clf.fit(tfidf_train, Y_train);
+pred= clf.predict(tfidf_test);
+score=accuracy_score(Y_test, pred );
+print("accuracy:   %0.3f" % score);
